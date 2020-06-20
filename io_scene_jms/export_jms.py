53f4fd8d2187e759c96ac5cfc85fd4720c299f54
Steven Garcia <stevengarcia55@ymail.com>
2020-06-20 07:10:21 -0500

    Changes to extension code
    Add an extension anyways if what the user has selected is different from what's currently on the file.

--- io_scene_jms/export_jms.py
+++ io_scene_jms/export_jms.py
@@ -498,7 +498,8 @@ def write_file(context, filepath, report, extension, jms_version, game_version, 
 
     extension_list = ['jms', 'jmp']
     true_extension = ''
-    if not filepath[-3:].lower() in extension_list:
+    extension_char = (len(extension)) - 1
+    if not filepath[-(extension_char):].lower() in extension_list or not filepath[-(extension_char):].lower() in extension.lower():      
         true_extension = extension
 
     file = open(filepath + true_extension, 'w', encoding='%s' % get_encoding(game_version))
