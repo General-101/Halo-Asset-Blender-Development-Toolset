88c1741a3a78502618e379e5a94de85c3cb77597
Steven Garcia <stevengarcia55@ymail.com>
2021-03-16 15:26:40 -0500

    Fix a crash and split names correctly
    Script was trying to subtract from none
    
    re.split didn't work like I expected it to. Should be good now I think?

--- io_scene_halo/misc/rename_bones.py
+++ io_scene_halo/misc/rename_bones.py
@@ -31,8 +31,8 @@ from io_scene_halo.global_functions import global_functions
 
 def rename_bones():
     node_prefix_tuple = ('b ', 'b_', 'bone ','bone_', 'frame ','frame_', 'bip01 ', 'bip01_')
-    blender_style = (".l", ".r") 
-    halo_style = ("l ", "r ", "l_", "r_") 
+    blender_style = (".l", ".r")
+    halo_style = ("l ", "r ", "l_", "r_")
 
     active_object = bpy.context.view_layer.objects.active
 
@@ -51,27 +51,42 @@ def rename_bones():
                     for prefix in node_prefix_tuple:
                         if bone.name.lower().startswith(prefix):
                             prefix_index = node_prefix_tuple.index(prefix)
-                            bone_name = re.split(prefix, bone.name, flags=re.IGNORECASE)[1]
+                            prefix_letter_list = list(prefix)
+                            prefix_limiter = ''
+                            for letter in prefix_letter_list:
+                                prefix_limiter += ("[{}]".format(letter))
+                            bone_name = re.split(prefix_limiter, bone.name, flags=re.IGNORECASE)[1]
                             for style in blender_style:
                                 if bone_name.lower().endswith(style):
-                                    bone_name = re.split(style, bone_name, flags=re.IGNORECASE)[0]
+                                    style_letter_list = list(style)
+                                    style_limiter = ''
+                                    for letter in style_letter_list:
+                                        style_limiter += ("[{}]".format(letter))
+                                    bone_name = re.split(style_limiter, bone_name, flags=re.IGNORECASE)[0]
                                     bone_side_index = blender_style.index(style)
                 else:
                     for style in blender_style:
                         if bone.name.lower().endswith(style):
-                            bone_name = re.split(style, bone.name, flags=re.IGNORECASE)[0]
+                            style_letter_list = list(style)
+                            style_limiter = ''
+                            for letter in style_letter_list:
+                                style_limiter += ("[{}]".format(letter))
+                            bone_name = re.split(style_limiter, bone.name, flags=re.IGNORECASE)[0]
                             bone_side_index = blender_style.index(style)
 
-
                 if not bone_side_index == None:
-                    bone.name = node_prefix_tuple[prefix_index] + halo_style[bone_side_index] + bone_name    
+                    bone.name = node_prefix_tuple[prefix_index] + halo_style[bone_side_index] + bone_name
 
             elif style_type == 1:
                 if bone.name.lower().startswith(node_prefix_tuple):
                     for prefix in node_prefix_tuple:
                         if bone.name.lower().startswith(prefix):
                             prefix_index = node_prefix_tuple.index(prefix)
-                            bone_name = re.split(prefix, bone.name, flags=re.IGNORECASE)[-1]
+                            prefix_letter_list = list(prefix)
+                            prefix_limiter = ''
+                            for letter in prefix_letter_list:
+                                prefix_limiter += ("[{}]".format(letter))
+                            bone_name = re.split(prefix_limiter, bone.name, flags=re.IGNORECASE)[-1]
                             for style in halo_style:
                                 if bone_name.lower().startswith(style):
                                     bone_name = re.split(style, bone_name, flags=re.IGNORECASE)[-1]
@@ -79,14 +94,18 @@ def rename_bones():
                 else:
                     for style in halo_style:
                         if bone_name.lower().startswith(style):
-                            bone_name = re.split(style, bone.name, flags=re.IGNORECASE)[-1]
-                            bone_side_index = halo_style.index(style)  
-
-                if bone_side_index > 1:
-                    bone_side_index = bone_side_index - 2
+                            style_letter_list = list(style)
+                            style_limiter = ''
+                            for letter in style_letter_list:
+                                style_limiter += ("[{}]".format(letter))
+                            bone_name = re.split(style_limiter, bone.name, flags=re.IGNORECASE)[-1]
+                            bone_side_index = halo_style.index(style)
 
                 if not bone_side_index == None:
-                    bone.name = node_prefix_tuple[prefix_index] + bone_name + blender_style[bone_side_index]   
+                    if bone_side_index > 1:
+                        bone_side_index = bone_side_index - 2
+
+                    bone.name = node_prefix_tuple[prefix_index] + bone_name + blender_style[bone_side_index]
 
     return {'FINISHED'}
 
