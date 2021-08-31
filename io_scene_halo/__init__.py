01aea3743a3cb8a8a8b2ecd751a56bbc76da68ef
num0005 <double.null@outlook.com>
2021-08-31 18:32:22 +0100

    Bump min Blender version to 2.83.17
    2.80.75 has a bug that causes a crash if you try to export while in edit mode, it's fixed in 2.83.17 which is also one of the current LTS versions.

--- io_scene_halo/__init__.py
+++ io_scene_halo/__init__.py
@@ -28,7 +28,7 @@ bl_info = {
     "name": "Halo Asset Blender Development Toolset",
     "author": "General_101",
     "version": (1, 0, 0),
-    "blender": (2, 80, 0),
+    "blender": (2, 83, 17),
     "location": "File > Import-Export",
     "description": "Import-Export Halo CE/2/3 Jointed Model Skeleton File (.jms), Import-Export Halo CE/2/3 Jointed Model Animation File (.jma), Import-Export Halo 2/3 Amalgam Scene Specification File (.ass), Import Halo CE Virtual Reality Modeling Language File (.wrl), and Export Halo 2 Vista H2Codez lightmap UV (.luv). Originally by Cyboryxmen with changes by Fulsy + MosesofEgypt + con for JMS portion. Initial ASS exporter by Dave Barnes (Aerial Dave). WRL importing originally by Con.",
     "warning": "",
