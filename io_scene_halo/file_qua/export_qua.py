d0e96c00a89b01c30091b89769bcfd6dc2f1a1e5
Steven Garcia <stevengarcia55@ymail.com>
2021-10-30 11:04:29 -0500

    Extra camera and audio data stuff

--- io_scene_halo/file_qua/export_qua.py
+++ io_scene_halo/file_qua/export_qua.py
@@ -68,13 +68,19 @@ class QUAScene(global_functions.HaloAsset):
             self.frames = frames
             self.audio_data = audio_data
 
-    class ExtraShots:
-        def __init__(self, frames, audio_data):
-            self.frames = frames
-            self.audio_data = audio_data
+    class ExtraCamera:
+        def __init__(self, name, type):
+            self.name = name
+            self.type = type
+
+    class ExtraCameras:
+        def __init__(self, camera, extra_shots):
+            self.camera = camera
+            self.extra_shots = extra_shots
 
     class Frames:
-        def __init__(self, position, up, forward, fov, aperture, focal_length, depth_of_field, near_focal, far_focal, focal_depth, blur_amount):
+        def __init__(self, camera_is_enabled, position, up, forward, fov, aperture, focal_length, depth_of_field, near_focal, far_focal, focal_depth, blur_amount):
+            self.camera_is_enabled = camera_is_enabled
             self.position = position
             self.up = up
             self.forward = forward
@@ -102,50 +108,105 @@ class QUAScene(global_functions.HaloAsset):
 
         self.version = version
         self.name = scene_name
-        self.shots = []
         self.units = []
         self.scenery = []
         self.effects_scenery = []
+        self.shots = []
         self.extra_cameras = []
-        self.extra_shots = []
 
-        first_frame = context.scene.frame_start
-        last_frame = context.scene.frame_end + 1
+        ubercam = None
+        extra_cameras = []
+        for obj in object_list:
+            name = obj.name.lower()
+            if obj.type == 'CAMERA':
+                if name[0:1] == '&':
+                    ubercam = obj
 
-        ubercam = bpy.data.objects.get("ubercam")
+                if name[0:1] == '+':
+                    extra_cameras.append(obj)
 
-        for action in bpy.data.actions:
-            ubercam.animation_data.action = action
-            first_frame = round(action.fcurves[0].keyframe_points[0].co[0])
-            last_frame = round(action.fcurves[0].keyframe_points[-1].co[0]) + 1
-            print(first_frame)
-            print(last_frame)
-                    
-            transforms_for_frame = []
-            for frame in range(first_frame, last_frame):
-                context.scene.frame_set(frame)
+        if ubercam:
+            for action in bpy.data.actions:    
+                action_prefix = action.name.split(' ', 1)[0]
+                action_suffix = action.name.split(' ', 1)[1]
+                if action_prefix.startswith(ubercam.name.split('&', 1)[1]):
+                    action_tranforms = []
+                    sound_data = []
+                    first_frame = round(action.fcurves[0].keyframe_points[0].co[0])
+                    last_frame = round(action.fcurves[0].keyframe_points[-1].co[0]) + 1         
+                    ubercam.animation_data.action = action
+                    for frame in range(first_frame, last_frame):
+                        context.scene.frame_set(frame)
 
-                camera_matrix = global_functions.get_matrix(ubercam, ubercam, False, None, None, False, version, 'QUA', False, 1)
-                mesh_dimensions = global_functions.get_dimensions(camera_matrix, ubercam, version, None, False, False, 'QUA', 1)
-                position = (mesh_dimensions.position[0], mesh_dimensions.position[1], mesh_dimensions.position[2])
+                        camera_matrix = global_functions.get_matrix(ubercam, ubercam, False, None, None, False, version, 'QUA', False, 1)
+                        mesh_dimensions = global_functions.get_dimensions(camera_matrix, ubercam, version, None, False, False, 'QUA', 1)
+                        position = (mesh_dimensions.position[0], mesh_dimensions.position[1], mesh_dimensions.position[2])
 
-                up_vector = camera_matrix.to_quaternion() @ Vector((0, 1, 0))
-                forward_vector = camera_matrix.to_quaternion() @ Vector((0, 0, -1))
+                        up_vector = camera_matrix.to_quaternion() @ Vector((0, 1, 0))
+                        forward_vector = camera_matrix.to_quaternion() @ Vector((0, 0, -1))
 
-                up = (up_vector[0], up_vector[1], up_vector[2])
-                forward = (forward_vector[0], forward_vector[1], forward_vector[2])
-                fov = degrees(2 * atan(ubercam.data.sensor_width /(2 * ubercam.data.lens)))
-                aperture = ubercam.data.dof.aperture_ratio
-                focal_length = ubercam.data.lens
-                depth_of_field = int(ubercam.data.dof.use_dof)
-                near_focal = ubercam.data.clip_start
-                far_focal = ubercam.data.clip_end
-                focal_depth = ubercam.data.dof.focus_distance
-                blur_amount = ubercam.data.dof.aperture_fstop
+                        camera_is_enabled = int(ubercam.hide_render)
+                        up = (up_vector[0], up_vector[1], up_vector[2])
+                        forward = (forward_vector[0], forward_vector[1], forward_vector[2])
+                        fov = degrees(2 * atan(ubercam.data.sensor_width /(2 * ubercam.data.lens)))
+                        aperture = ubercam.data.dof.aperture_ratio
+                        focal_length = ubercam.data.lens
+                        depth_of_field = int(ubercam.data.dof.use_dof)
+                        near_focal = ubercam.data.clip_start
+                        far_focal = ubercam.data.clip_end
+                        focal_depth = ubercam.data.dof.focus_distance
+                        blur_amount = ubercam.data.dof.aperture_fstop
 
-                transforms_for_frame.append(QUAScene.Frames(position, up, forward, fov, aperture, focal_length, depth_of_field, near_focal, far_focal, focal_depth, blur_amount))
+                        action_tranforms.append(QUAScene.Frames(camera_is_enabled, position, up, forward, fov, aperture, focal_length, depth_of_field, near_focal, far_focal, focal_depth, blur_amount))
 
-            self.shots.append(QUAScene.Shots(transforms_for_frame, []))
+                        for sequence in context.scene.sequence_editor.sequences_all:
+                            if sequence.name.startswith(action_suffix) and sequence.frame_start == frame:
+                                sound_data.append(QUAScene.AudioData(sequence.sound.filepath, frame, sequence.sound.name))
+
+                    self.shots.append(QUAScene.Shots(action_tranforms, sound_data))
+
+            for extra_camera_obj in extra_cameras:
+                extra_camera = None
+                extra_shots = []
+                for action in bpy.data.actions:         
+                    action_prefix = action.name.split(' ', 1)[0]
+                    if action_prefix.startswith(extra_camera_obj.name.split('+', 1)[1]):
+                        extra_camera = QUAScene.ExtraCamera(extra_camera_obj.name, "")
+                        extra_action_tranforms = []
+                        first_frame = round(action.fcurves[0].keyframe_points[0].co[0])
+                        last_frame = round(action.fcurves[0].keyframe_points[-1].co[0]) + 1      
+                        extra_camera_obj.animation_data.action = action
+                        for frame in range(first_frame, last_frame):
+                            context.scene.frame_set(frame)
+
+                            camera_matrix = global_functions.get_matrix(ubercam, ubercam, False, None, None, False, version, 'QUA', False, 1)
+                            mesh_dimensions = global_functions.get_dimensions(camera_matrix, ubercam, version, None, False, False, 'QUA', 1)
+                            position = (mesh_dimensions.position[0], mesh_dimensions.position[1], mesh_dimensions.position[2])
+
+                            up_vector = camera_matrix.to_quaternion() @ Vector((0, 1, 0))
+                            forward_vector = camera_matrix.to_quaternion() @ Vector((0, 0, -1))
+
+                            camera_is_enabled = int(extra_camera_obj.hide_render)
+                            up = (up_vector[0], up_vector[1], up_vector[2])
+                            forward = (forward_vector[0], forward_vector[1], forward_vector[2])
+                            fov = degrees(2 * atan(ubercam.data.sensor_width /(2 * ubercam.data.lens)))
+                            aperture = ubercam.data.dof.aperture_ratio
+                            focal_length = ubercam.data.lens
+                            depth_of_field = int(ubercam.data.dof.use_dof)
+                            near_focal = ubercam.data.clip_start
+                            far_focal = ubercam.data.clip_end
+                            focal_depth = ubercam.data.dof.focus_distance
+                            blur_amount = ubercam.data.dof.aperture_fstop
+
+                            extra_action_tranforms.append(QUAScene.Frames(camera_is_enabled, position, up, forward, fov, aperture, focal_length, depth_of_field, near_focal, far_focal, focal_depth, blur_amount))
+
+                        extra_shots.append(QUAScene.Shots(extra_action_tranforms, []))
+
+                if extra_camera:
+                    self.extra_cameras.append(QUAScene.ExtraCameras(extra_camera, extra_shots))
+
+        else:
+            raise global_functions.SceneParseError("No cameras in your scene. Create a camera and give it & as the prefix for the object name")
 
 
 def write_file(context, filepath, report, version):
@@ -224,7 +285,7 @@ def write_file(context, filepath, report, version):
 
     for idx, shot in enumerate(qua_scene.shots):
         file.write(
-            '\n; ### SHOT %s ###' % (idx) +
+            '\n; ### SHOT %s ###' % (idx + 1) +
             '\n;          <Ubercam position (vector)>' +
             '\n;          <Ubercam up (vector)>' +
             '\n;          <Ubercam forward (vector)>' +
@@ -241,7 +302,7 @@ def write_file(context, filepath, report, version):
 
         for idx, frame in enumerate(shot.frames):
             file.write(
-                '\n; FRAME %s' % (idx) +
+                '\n; FRAME %s' % (idx + 1) +
                 decimal_3 % (frame.position) +
                 decimal_3 % (frame.up) +
                 decimal_3 % (frame.forward) +
@@ -279,46 +340,45 @@ def write_file(context, filepath, report, version):
         '\n;          <Camera type (string)>\n'
     )
 
-    for idx, extra_camera in enumerate(qua_scene.extra_cameras):
+    for idx, camera_data in enumerate(qua_scene.extra_cameras):
         file.write(
             '\n;### CAMERA %s ###' % (idx) +
-            '\n%s' % (extra_camera.name) +
-            '\n%s\n' % (extra_camera.type)
+            '\n%s' % (camera_data.camera.name) +
+            '\n%s\n' % (camera_data.camera.type)
         )
 
-    for idx, extra_shot in enumerate(qua_scene.extra_shots):
-        file.write(
-            '\n; ### SHOT %s ###' % (idx) +
-            '\n;          <Ubercam position (vector)>' +
-            '\n;          <Ubercam up (vector)>' +
-            '\n;          <Ubercam forward (vector)>' +
-            '\n;          <Horizontal field of view (float)>' +
-            '\n;          <Horizontal film aperture (float, millimeters)>' +
-            '\n;          <Focal Length (float)>' +
-            '\n;          <Depth of Field (bool)>' +
-            '\n;          <Near Focal Plane Distance (float)>' +
-            '\n;          <Far Focal Plane Distance (float)>' +
-            '\n;          <Focal Depth (float)>' +
-            '\n;          <Blur Amount (float)>' +
-            '\n%s' % (len(extra_shot.frames))
-        )
-
-        for idx, frame in enumerate(extra_shot):
+        for idx, extra_shot in enumerate(camera_data.extra_shots):
             file.write(
-                '\n; FRAME %s' % (idx) +
-                decimal_3 % (frame.position) +
-                decimal_3 % (frame.up) +
-                decimal_3 % (frame.forward) +
-                decimal_1 % (frame.fov) +
-                decimal_1 % (frame.aperture) +
-                decimal_1 % (frame.focal_length) +
-                '\n%s' % (frame.depth_of_field) +
-                decimal_1 % (frame.near_focal) +
-                decimal_1 % (frame.far_focal) +
-                decimal_1 % (frame.focal_depth) +
-                decimal_1 % (frame.blur_amount) +
-                '\n'
+                '\n; ### SHOT %s ###' % (idx + 1) +
+                '\n;          <Camera enabled (bool)>' +
+                '\n;          <Camera position (vector)>' +
+                '\n;          <Camera up (vector)>' +
+                '\n;          <Camera forward (vector)>' +
+                '\n;          <Horizontal field of view (float)>' +
+                '\n;          <Focal Length (float)>' +
+                '\n;          <Depth of Field (bool)>' +
+                '\n;          <Near Focal Plane Distance (float)>' +
+                '\n;          <Far Focal Plane Distance (float)>' +
+                '\n;          <Focal Depth (float)>' +
+                '\n;          <Blur Amount (float)>'
             )
+
+            for idx, frame in enumerate(extra_shot.frames):
+                file.write(
+                    '\n; FRAME %s' % (idx + 1) +
+                    '\n%s' % (frame.camera_is_enabled) +
+                    decimal_3 % (frame.position) +
+                    decimal_3 % (frame.up) +
+                    decimal_3 % (frame.forward) +
+                    decimal_1 % (frame.fov) +
+                    decimal_1 % (frame.focal_length) +
+                    '\n%s' % (frame.depth_of_field) +
+                    decimal_1 % (frame.near_focal) +
+                    decimal_1 % (frame.far_focal) +
+                    decimal_1 % (frame.focal_depth) +
+                    decimal_1 % (frame.blur_amount) +
+                    '\n'
+                )
 
     file.close()
     report({'INFO'}, "Export completed successfully")
