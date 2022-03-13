758fd12b887a4b43684a41e1810955303c24483f
SamDamDing <45549722+SamDamDing@users.noreply.github.com>
2022-03-13 12:58:34 -0400

    updated license

--- io_scene_halo/misc/mattools.py
+++ io_scene_halo/misc/mattools.py
@@ -2,7 +2,7 @@
 #
 # MIT License
 #
-# Copyright (c) 2021 Steven Garcia
+# Copyright (c) 2021 SamDamDing AKA MercyMoon
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
