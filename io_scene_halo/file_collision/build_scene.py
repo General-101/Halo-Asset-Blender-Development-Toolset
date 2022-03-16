35924e25d6ea3ab8f7c00c94dbb40d0377814d40
Steven Garcia <stevengarcia55@ymail.com>
2022-03-16 18:47:09 -0500

    Generate surfaces with +unused material for empty collision nodes

--- io_scene_halo/file_collision/build_scene.py
+++ io_scene_halo/file_collision/build_scene.py
@@ -28,7 +28,7 @@ import bpy
 import bmesh
 
 from math import radians
-from mathutils import Matrix
+from mathutils import Matrix, Vector
 from ..global_functions import mesh_processing, global_functions
 
 def build_pathfinding_spheres(context, armature, COLLISION, fix_rotations):
@@ -89,47 +89,115 @@ def build_collision(context, armature, COLLISION):
     for node_idx, node in enumerate(COLLISION.nodes):
         parent_name = node.name
         for bsp_idx, bsp in enumerate(node.bsps):
-            active_region_permutations = []
+            if len(bsp.surfaces) > 0:
+                active_region_permutations = []
 
-            region = COLLISION.regions[node.region]
-            region_name = region.name
-            permutation_name = region.permutations[bsp_idx].name
+                region = COLLISION.regions[node.region]
+                region_name = region.name
+                permutation_name = region.permutations[bsp_idx].name
 
-            if region_name == "__unnamed":
-                region_name = "unnamed"
+                if region_name == "__unnamed":
+                    region_name = "unnamed"
 
-            if permutation_name == "__base":
-                permutation_name = "base"
+                if permutation_name == "__base":
+                    permutation_name = "base"
 
-            object_name = '@%s %s %s' % (region_name, permutation_name, node.name)
-            bm = bmesh.new()
+                object_name = '@%s %s %s' % (region_name, permutation_name, node.name)
+                bm = bmesh.new()
 
-            mesh = bpy.data.meshes.new(object_name)
-            object_mesh = bpy.data.objects.new(object_name, mesh)
-            collection.objects.link(object_mesh)
+                mesh = bpy.data.meshes.new(object_name)
+                object_mesh = bpy.data.objects.new(object_name, mesh)
+                collection.objects.link(object_mesh)
 
-            for surface_idx, surface in enumerate(bsp.surfaces):
-                edge_index = surface.first_edge
-                surface_edges = []
-                vert_indices = []
-                while edge_index not in surface_edges:
-                    surface_edges.append(edge_index)
-                    edge = bsp.edges[edge_index]
-                    if edge.left_surface == surface_idx:
-                        vert_indices.append(bm.verts.new(bsp.vertices[edge.start_vertex].translation))
-                        edge_index = edge.forward_edge
+                for surface_idx, surface in enumerate(bsp.surfaces):
+                    edge_index = surface.first_edge
+                    surface_edges = []
+                    vert_indices = []
+                    while edge_index not in surface_edges:
+                        surface_edges.append(edge_index)
+                        edge = bsp.edges[edge_index]
+                        if edge.left_surface == surface_idx:
+                            vert_indices.append(bm.verts.new(bsp.vertices[edge.start_vertex].translation))
+                            edge_index = edge.forward_edge
 
-                    else:
-                        vert_indices.append(bm.verts.new(bsp.vertices[edge.end_vertex].translation))
-                        edge_index = edge.reverse_edge
+                        else:
+                            vert_indices.append(bm.verts.new(bsp.vertices[edge.end_vertex].translation))
+                            edge_index = edge.reverse_edge
 
+                    bm.faces.new(vert_indices)
+
+                bm.faces.ensure_lookup_table()
+                for surface_idx, surface in enumerate(bsp.surfaces):
+                    ngon_material_index = surface.material
+                    if not ngon_material_index == -1:
+                        mat = COLLISION.materials[ngon_material_index]
+
+                    current_region_permutation = region_name
+
+                    if not current_region_permutation in active_region_permutations:
+                        active_region_permutations.append(current_region_permutation)
+                        object_mesh.face_maps.new(name=current_region_permutation)
+
+                    if not ngon_material_index == -1:
+                        material_list = []
+
+                        material_name = mat.name
+                        mat = bpy.data.materials.get(material_name)
+                        if mat is None:
+                            mat = bpy.data.materials.new(name=material_name)
+
+                        for slot in object_mesh.material_slots:
+                            material_list.append(slot.material)
+
+                        if not mat in material_list:
+                            material_list.append(mat)
+                            object_mesh.data.materials.append(mat)
+
+                        mat.diffuse_color = random_color_gen.next()
+                        material_index = material_list.index(mat)
+                        bm.faces[surface_idx].material_index = material_index
+
+                    fm = bm.faces.layers.face_map.verify()
+                    face_idx = bm.faces[surface_idx]
+                    face_idx[fm] = active_region_permutations.index(current_region_permutation)
+
+                bm.to_mesh(mesh)
+                bm.free()
+
+                mesh_processing.select_object(context, object_mesh)
+                mesh_processing.select_object(context, armature)
+                bpy.ops.object.mode_set(mode='EDIT')
+                armature.data.edit_bones.active = armature.data.edit_bones[parent_name]
+                bpy.ops.object.mode_set(mode='OBJECT')
+                bpy.ops.object.parent_set(type='BONE', keep_transform=True)
+                object_mesh.matrix_world = armature.pose.bones[parent_name].matrix
+                object_mesh.select_set(False)
+                armature.select_set(False)
+
+            else:
+                active_region_permutations = []
+
+                region = COLLISION.regions[node.region]
+                region_name = region.name
+                permutation_name = region.permutations[bsp_idx].name
+
+                if region_name == "__unnamed":
+                    region_name = "unnamed"
+
+                if permutation_name == "__base":
+                    permutation_name = "base"
+
+                object_name = '@%s %s %s' % (region_name, permutation_name, node.name)
+                bm = bmesh.new()
+
+                mesh = bpy.data.meshes.new(object_name)
+                object_mesh = bpy.data.objects.new(object_name, mesh)
+                collection.objects.link(object_mesh)
+
+
+                vert_indices = [bm.verts.new(Vector((0,0,0))), bm.verts.new(Vector((1,0,1))), bm.verts.new(Vector((1,0,0)))]
                 bm.faces.new(vert_indices)
-
-            bm.faces.ensure_lookup_table()
-            for surface_idx, surface in enumerate(bsp.surfaces):
-                ngon_material_index = surface.material
-                if not ngon_material_index == -1:
-                    mat = COLLISION.materials[ngon_material_index]
+                bm.faces.ensure_lookup_table()
 
                 current_region_permutation = region_name
 
@@ -140,7 +208,7 @@ def build_collision(context, armature, COLLISION):
                 if not ngon_material_index == -1:
                     material_list = []
 
-                    material_name = mat.name
+                    material_name = "+unused"
                     mat = bpy.data.materials.get(material_name)
                     if mat is None:
                         mat = bpy.data.materials.new(name=material_name)
@@ -154,24 +222,24 @@ def build_collision(context, armature, COLLISION):
 
                     mat.diffuse_color = random_color_gen.next()
                     material_index = material_list.index(mat)
-                    bm.faces[surface_idx].material_index = material_index
+                    bm.faces[0].material_index = material_index
 
                 fm = bm.faces.layers.face_map.verify()
-                face_idx = bm.faces[surface_idx]
+                face_idx = bm.faces[0]
                 face_idx[fm] = active_region_permutations.index(current_region_permutation)
 
-            bm.to_mesh(mesh)
-            bm.free()
+                bm.to_mesh(mesh)
+                bm.free()
 
-            mesh_processing.select_object(context, object_mesh)
-            mesh_processing.select_object(context, armature)
-            bpy.ops.object.mode_set(mode='EDIT')
-            armature.data.edit_bones.active = armature.data.edit_bones[parent_name]
-            bpy.ops.object.mode_set(mode='OBJECT')
-            bpy.ops.object.parent_set(type='BONE', keep_transform=True)
-            object_mesh.matrix_world = armature.pose.bones[parent_name].matrix
-            object_mesh.select_set(False)
-            armature.select_set(False)
+                mesh_processing.select_object(context, object_mesh)
+                mesh_processing.select_object(context, armature)
+                bpy.ops.object.mode_set(mode='EDIT')
+                armature.data.edit_bones.active = armature.data.edit_bones[parent_name]
+                bpy.ops.object.mode_set(mode='OBJECT')
+                bpy.ops.object.parent_set(type='BONE', keep_transform=True)
+                object_mesh.matrix_world = armature.pose.bones[parent_name].matrix
+                object_mesh.select_set(False)
+                armature.select_set(False)
 
 def build_scene(context, COLLISION, fix_rotations, report):
     active_object = context.view_layer.objects.active
