24c4416a9f7c1310955ea0ff698b5bdc071b6368
Crisp <80323148+ILoveAGoodCrisp@users.noreply.github.com>
2022-09-23 15:40:56 +0100

    gr2 init rewrite - building GUI

--- io_scene_halo/file_gr2/__init__.py
+++ io_scene_halo/file_gr2/__init__.py
@@ -37,8 +37,8 @@ from mimetypes import init
 import bpy
 from bpy_extras.io_utils import ExportHelper
 from bpy.props import StringProperty, BoolProperty, EnumProperty
-from bpy.types import Operator
-from bpy_extras.io_utils import ExportHelper
+from bpy.types import Operator, Panel
+from bpy_extras.io_utils import ExportHelper, orientation_helper
 
 import os
 t = os.getcwd()
@@ -47,12 +47,12 @@ print(t)
 import sys
 sys.modules[bpy.types.IMPORT_SCENE_OT_fbx.__module__].__file__
 from subprocess import run
-#run('python ' + "\"" + str(sys.modules[bpy.types.IMPORT_SCENE_OT_fbx.__module__].__file__) + "\"")
 import sys
 sys.path.insert(0,t)
 from io_scene_fbx import export_fbx_bin, __init__
 print("HERE!!!")
-#bpy.ops.IMPORT_SCENE_OT_fbx.FBX_PT_import_transform(bpy.types.Panel)
+
+@orientation_helper(axis_forward='Y', axis_up='Z')
 
 class Export_Halo_GR2(Operator, ExportHelper):
     """Writes a Halo Reach GR2 File using your Halo Editing Kit"""
@@ -63,22 +63,29 @@ class Export_Halo_GR2(Operator, ExportHelper):
     filter_glob: StringProperty(
         default="*.gr2",
         options={'HIDDEN'},
-        maxlen=255,  # Max internal buffer length, longer would be clamped.
+        maxlen=255,
     )
+
+    game_version:EnumProperty(
+        name="Game Version",
+        description="The game to export this asset for",
+        items=[ ('REACH', "Halo Reach", "Export an asset intended for Halo Reach"),
+            ]
+        )
 
     export_gr2: BoolProperty(
             name="Export To GR2",
             description="Uses the exported FBX and JSON files to generate a GR2 File",
             default=True,
             )
-    delete_fbx: BoolProperty(
+    keep_fbx: BoolProperty(
             name="FBX",
-            description="Delete the source FBX file after GR2 conversion",
+            description="Keep the source FBX file after GR2 conversion",
             default=False,
             )
-    delete_json: BoolProperty(
+    keep_json: BoolProperty(
             name="JSON",
-            description="Delete the source JSON file after GR2 conversion",
+            description="Keep the source JSON file after GR2 conversion",
             default=False,
             )
     export_sidecar: BoolProperty(
@@ -96,104 +103,256 @@ class Export_Halo_GR2(Operator, ExportHelper):
                 ('PARTICLE MODEL', "Particle Model", ""),
                ]
         )
+    export_method: EnumProperty(
+            name="Export Method",
+            description="",
+            items=[('BATCH', "Batch", ""), ('SELECTED', "Selected", "")]
+            )
+    export_animations: BoolProperty(
+            name='Animations',
+            description='',
+            default=True,
+        )
+    export_render: BoolProperty(
+            name='Render Models',
+            description='',
+            default=True,
+        )
+    export_collision: BoolProperty(
+            name='Collision Models',
+            description='',
+            default=True,
+        )
+    export_physics: BoolProperty(
+            name='Physics Models',
+            description='',
+            default=True,
+        )
+    export_markers: BoolProperty(
+            name='Markers',
+            description='',
+            default=True,
+        )
+    export_structure: BoolProperty(
+            name='Structure',
+            description='',
+            default=True,
+        )
+    export_structure_design: BoolProperty(
+            name='Structure Design',
+            description='',
+            default=True,
+        )
+    output_biped: BoolProperty(
+            name='Biped',
+            description='',
+            default=False,
+    )
+    output_crate: BoolProperty(
+            name='Crate',
+            description='',
+            default=False,
+    )
+    output_creature: BoolProperty(
+            name='Creature',
+            description='',
+            default=False,
+    )
+    output_device_control: BoolProperty(
+            name='Device Control',
+            description='',
+            default=False,
+    )
+    output_device_machine: BoolProperty(
+            name='Device Machine',
+            description='',
+            default=False,
+    )
+    output_device_terminal: BoolProperty(
+            name='Device Terminal',
+            description='',
+            default=False,
+    )
+    output_effect_scenery: BoolProperty(
+            name='Effect Scenery',
+            description='',
+            default=False,
+    )
+    output_equipment: BoolProperty(
+            name='Equipment',
+            description='',
+            default=False,
+    )
+    output_giant: BoolProperty(
+            name='Giant',
+            description='',
+            default=False,
+    )
+    output_scenery: BoolProperty(
+            name='Scenery',
+            description='',
+            default=False,
+    )
+    output_vehicle: BoolProperty(
+            name='Vehicle',
+            description='',
+            default=False,
+    )
+    output_weapon: BoolProperty(
+            name='Weapon',
+            description='',
+            default=False,
+    )
+    import_to_game: BoolProperty(
+            name='Import to Game',
+            description='',
+            default=False,
+    )
+    import_check: BoolProperty(
+            name='Check',
+            description='Run the import process but produce no output files',
+            default=False,
+    )
+    import_force: BoolProperty(
+            name='Force',
+            description="Force all files to import even if they haven't changed",
+            default=False,
+    )
+    import_verbose: BoolProperty(
+            name='Verbose',
+            description="Write additional import progress information to the console",
+            default=False,
+    )
+    import_draft: BoolProperty(
+            name='Draft',
+            description="Skip generating PRT data. Faster speed, lower quality",
+            default=False,
+    )
+    import_seam_debug: BoolProperty(
+            name='Seam Debug',
+            description="Write extra seam debugging information to the console",
+            default=False,
+    )
+    import_skip_instances: BoolProperty(
+            name='Skip Instances',
+            description="Skip importing all instanced geometry",
+            default=False,
+    )
+    import_decompose_instances: BoolProperty(
+            name='Decompose Instances',
+            description="Run convex decomposition for instanced geometry physics (very slow)",
+            default=False,
+    )
+    import_surpress_errors: BoolProperty(
+            name='Surpress Errors',
+            description="Do not write errors to vrml files",
+            default=False,
+    )
+
     asset_path: StringProperty(
             name="Asset Folder Path",
             description="",
             )
+    apply_unit_scale: BoolProperty(
+            name="Apply Unit",
+            description="",
+            default=True,
+            )
+    apply_scale_options: EnumProperty(
+            default='FBX_SCALE_UNITS',
+            items=[('FBX_SCALE_UNITS', "FBX Units Scale",""),]
+            )
+    use_selection: BoolProperty(
+            name="Selected Objects",
+            description="Export selected and visible objects only",
+            default=False,
+            )
 
     def execute(self, context):
+
+        keywords = self.as_keywords()
+
         from . import export_gr2, export_sidecar_xml
         export_fbx_bin.save(self, context, **keywords)
         export_gr2.save(self, context, self.report, **keywords)
         return export_sidecar_xml.save(self, context, self.report, **keywords)
 
     def draw(self, context):
-        pass
+        scene = context.scene
+        layout = self.layout
+        layout.use_property_split = True
+        box = layout.box()
+        # SETTINGS #
+        box.label(text="Settings")
+        
+        col = box.column()
+        col.prop(self, "game_version", text='Game Version')
+        col.prop(self, "export_method", text='Export Method')
+        col.prop(self, "sidecar_type", text='Asset Type')
+
+        sub = box.column(heading="Keep")
+        sub.prop(self, "keep_fbx")
+        sub.prop(self, "keep_json")
+        # EXPORT CATEGORIES #
+        box = layout.box()
+        box.label(text="Export Categories")
+        sub = box.column(heading="Export")
+        if self.sidecar_type == 'MODEL':
+            sub.prop(self, "export_animations")
+            sub.prop(self, "export_render")
+            sub.prop(self, "export_collision")
+            sub.prop(self, "export_physics")
+            sub.prop(self, "export_markers")
+        elif self.sidecar_type == 'SCENARIO':
+            sub.prop(self, "export_structure")
+            sub.prop(self, "export_structure_design")
+        else:
+            sub.prop(self, "export_render")
+
+        # SIDECAR SETTINGS #
+        box = layout.box()
+        box.label(text="Sidecar Settings")
+        col = box.column()
+        col.prop(self, "export_sidecar")
+        if self.sidecar_type == 'MODEL' and self.export_sidecar:
+            sub = box.column(heading="Output Tags")
+            if self.sidecar_type == 'MODEL':
+                sub.prop(self, "output_biped")
+                sub.prop(self, "output_crate")
+                sub.prop(self, "output_creature")
+                sub.prop(self, "output_device_control")
+                sub.prop(self, "output_device_machine")
+                sub.prop(self, "output_device_terminal")
+                sub.prop(self, "output_effect_scenery")
+                sub.prop(self, "output_equipment")
+                sub.prop(self, "output_giant")
+                sub.prop(self, "output_scenery")
+                sub.prop(self, "output_vehicle")
+                sub.prop(self, "output_weapon")
+        # IMPORT SETTINGS #
+        box = layout.box()
+        box.label(text="Import Settings")
+        col = box.column()
+        col.prop(self, "import_to_game")
+        if self.import_to_game:
+            sub = box.column(heading="Import Flags")
+            sub.prop(self, "import_check")
+            sub.prop(self, "import_force")
+            sub.prop(self, "import_verbose")
+            sub.prop(self, "import_surpress_errors")
+            if self.sidecar_type == 'SCENARIO':
+                sub.prop(self, "import_seam_debug")
+                sub.prop(self, "import_skip_instances")
+                sub.prop(self, "import_decompose_instances")
+            else:
+                sub.prop(self, "import_draft")
+                
 
 def menu_func_export(self, context):
     self.layout.operator(Export_Halo_GR2.bl_idname, text="Halo Granny File (.gr2)")
 
-class Halo_GR2_Settings(bpy.types.Panel):
-    bl_space_type = 'FILE_BROWSER'
-    bl_region_type = 'TOOL_PROPS'
-    bl_label = "Generate GR2 File"
-    bl_parent_id = "FILE_PT_operator"
-
-    @classmethod
-    def poll(cls, context):
-        sfile = context.space_data
-        operator = sfile.active_operator
-
-        return operator.bl_idname == "EXPORT_HALO_OT_gr2"
-
-    def draw_header(self, context):
-        sfile = context.space_data
-        operator = sfile.active_operator
-
-        self.layout.prop(operator, "export_gr2", text='')
-        import sys
-        sys.path.insert(0,t)
-        from io_scene_fbx import ExportFBX, FBX_PT_export_main, FBX_PT_export_include, FBX_PT_export_transform, FBX_PT_export_geometry, FBX_PT_export_armature, FBX_PT_export_bake_animation
-        ExportFBX(self)
-        FBX_PT_export_main(self)
-        FBX_PT_export_include(self)
-        FBX_PT_export_transform(self)
-
-    def draw(self, context):
-        layout = self.layout
-        layout.use_property_split = True
-        layout.use_property_decorate = False
-
-        sfile = context.space_data
-        operator = sfile.active_operator
-
-        layout.enabled = operator.export_gr2
-
-        sublayout = layout.column(heading="Clean Up Files")
-        sublayout.prop(operator, "delete_fbx")
-        sublayout.prop(operator, "delete_json")
-
-class Halo_Sidecar_Settings(bpy.types.Panel):
-    bl_space_type = 'FILE_BROWSER'
-    bl_region_type = 'TOOL_PROPS'
-    bl_label = "Generate Sidecar"
-    bl_parent_id = "FILE_PT_operator"
-
-    @classmethod
-    def poll(cls, context):
-        sfile = context.space_data
-        operator = sfile.active_operator
-
-        return operator.bl_idname == "EXPORT_HALO_OT_gr2"
-
-    def draw_header(self, context):
-        file = context.space_data
-        operator = file.active_operator
-
-        self.layout.prop(operator, "export_sidecar", text='')
-
-    def draw(self, context):
-        layout = self.layout
-        layout.use_property_split = True
-        layout.use_property_decorate = False
-
-        sfile = context.space_data
-        operator = sfile.active_operator
-
-        layout.enabled = operator.export_sidecar
-
-        flow = layout.grid_flow(row_major=True, columns=0, even_columns=True, even_rows=False, align=False)
-        col = flow.column()
-
-        col.prop(operator, "sidecar_type", text='Asset Type')
-        col.prop(operator, 'asset_path', text='Asset Folder')
-
 classes = (
     Export_Halo_GR2,
-    Halo_GR2_Settings,
-    Halo_Sidecar_Settings,
-    #bpy.ops.IMPORT_SCENE_OT_fbx.FBX_PT_import_transform,
-    #bpy.ops.IMPORT_SCENE_OT_fbx.FBX_PT_import_include,
 )
 
 def register():
