367fdc9a2d9e4b0981fea5d0c99697d171287637
Crisp <80323148+ILoveAGoodCrisp@users.noreply.github.com>
2023-02-19 02:57:16 +0000

    JMS helper tool updates
    Now no longer renames a mesh if it only has one facemap
    Updated to work on all selected objects

--- io_scene_halo/misc/GR2/jms_helper.py
+++ io_scene_halo/misc/GR2/jms_helper.py
@@ -26,9 +26,22 @@
 
 import bpy
 
+from ...file_gr2.nwo_utils import get_perm, deselect_all_objects, set_active_object
+
 def jms_assign(context, report):
+    jms_count = 0
+    for obj in context.selected_objects:
+        deselect_all_objects()
+        jms_count += jms_process(context, report, obj)
+        bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
+
+    report({'INFO'},f"Split {jms_count} meshes by face maps and updated regions & permutations")
+    return {'FINISHED'}
+
+def jms_process(context, report, obj):
     # Split object split by face map
-    obj = context.active_object
+    obj.select_set(True)
+    set_active_object(obj)
     mesh_name = str(obj.name)
     if obj.type == 'MESH' and len(obj.face_maps) > 0:
         if obj.name.startswith('@'):
@@ -38,6 +51,7 @@ def jms_assign(context, report):
         else:
             prefix = ''
         bpy.ops.object.mode_set(mode='EDIT', toggle=False)
+        skip_rename = len(obj.face_maps) == 1
         while len(obj.face_maps) > 1:
             # Deselect all faces except those in the current face map
             bpy.ops.mesh.select_all(action='DESELECT')
@@ -62,28 +76,28 @@ def jms_assign(context, report):
                 ob.select_set(True)
                 context.view_layer.objects.active = ob
                 bpy.ops.object.mode_set(mode='EDIT', toggle=False)
-                faces = ob.data.polygons
-                for f in faces:                   
-                    f.select = False
-                index = 0 
-                for _ in range(len(ob.face_maps)):
-                    ob.face_maps.active_index = index
-                    bpy.ops.object.face_map_select()
-                    if ob.data.count_selected_items()[2] > 0:
-                        bpy.ops.object.face_map_deselect()
-                        index +=1
-                    else:
-                        if len(obj.face_maps) > 1:
+                if len(obj.face_maps) > 1:
+                    faces = ob.data.polygons
+                    for f in faces:                   
+                        f.select = False
+                    index = 0 
+                    for _ in range(len(ob.face_maps)):
+                        ob.face_maps.active_index = index
+                        bpy.ops.object.face_map_select()
+                        if ob.data.count_selected_items()[2] > 0:
+                            bpy.ops.object.face_map_deselect()
+                            index +=1
+                        else:
                             bpy.ops.object.face_map_remove()
-                    
-                bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
-                ob.select_set(False)
+                        
+                    bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
+                    ob.select_set(False)
 
         for ob in new_selection:
             ob.select_set(True)
 
-        # for each mesh set the name to match the AMF naming convention
-        for ob in context.selected_objects:
+        # for each mesh set the name to match the AMF naming convention (except if only one facemap exists)
+        for ob in new_selection:
             if ob.type == 'MESH':
                 fm_name = ob.face_maps.active.name
                 fm_split = fm_name.split()
@@ -101,14 +115,16 @@ def jms_assign(context, report):
                     region = 'default'
                 
                 # Set the mesh name
-                ob.name = f'{prefix}{region}:{permutation}'
+                if not skip_rename:
+                    ob.name = f'{prefix}{region}:{permutation}'
                 # Set the mesh regions / permutations up
                 ob.nwo.Region_Name = region
                 ob.nwo.Permutation_Name = permutation
             
         report({'INFO'},f"Split {mesh_name} by face maps and updated regions & permutations")
+        return 1
     else:
         report({'WARNING'},f"{mesh_name} has no face maps")
+        return 0
 
-    return {'FINISHED'}
 
