b82f95cdf48a1d4839f6f08f8eace1e586f749d1
Steven Garcia <stevengarcia55@ymail.com>
2024-10-11 11:54:47 -0500

    Readd vertex normal grab thing

--- io_scene_halo/file_jms/process_scene.py
+++ io_scene_halo/file_jms/process_scene.py
@@ -257,6 +257,9 @@ def process_scene(context, version, game_version, generate_checksum, fix_rotatio
 
                     region = region_index
                     normal = evaluted_mesh.corner_normals[loop_index].vector.normalized()
+                    if not loop_normals:
+                        normal = (original_geo_matrix.to_3x3() @ vertex_data.normal).normalized()
+
                     scaled_translation = mesh_processing.process_mesh_export_vert(vertex_data, "JMS", original_geo_matrix, custom_scale)
                     uv_set = mesh_processing.process_mesh_export_uv(evaluted_mesh, "JMS", loop_index, version)
                     color = mesh_processing.process_mesh_export_color(evaluted_mesh, loop_index, point_idx)
